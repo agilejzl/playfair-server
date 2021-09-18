b1dbd52807eec73ce4f5ffec403cb24c01c37581
Teddy Jiang <jiangzhilong@kdanmobile.com>
2021-09-18 12:12:29 +0800

    Added playfair

---
+++ controllers/service.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+  beego "github.com/beego/beego/v2/server/web"
+  "bytes"
+  "fmt"
+  "os"
+  "os/exec"
+  "strings"
+)
+
+type ServiceController struct {
+  beego.Controller
+}
+
+type ServiceResp struct {
+  Result string `json:"result"`
+  ErrMsg string `json:"errorMsg"`
+}
+
+func CmdAndChangeDir(dir string, commandName string, params []string) (string, error) {
+  cmd := exec.Command(commandName, params...)
+  if len(dir) > 0 {
+    cmd.Dir = dir
+    fmt.Println("dir", dir, "Cmd: ", cmd.Args)
+  } else {
+    fmt.Println("Cmd: ", cmd.Args)
+  }
+
+  var out bytes.Buffer
+  cmd.Stdout = &out
+  cmd.Stderr = os.Stderr
+
+  err := cmd.Start()
+  if err != nil {
+    fmt.Println("Cmd error: ", err.Error())
+    return "", err
+  }
+  err = cmd.Wait()
+  outStr := out.String()
+  fmt.Println("Cmd out: ", len(outStr), outStr)
+  return outStr, err
+}
+
+func (c *ServiceController) Post() {
+  worker := c.GetString("worker")
+  param := c.GetString("param")
+  fmt.Println(worker, " param: ", param)
+  var params = strings.Split(param, " ")
+
+  var sResp = ServiceResp{}
+  // var params = []string { "-s", "46504c590301010000000004020002bb", "16" }
+  Result, err := CmdAndChangeDir("playfair", worker, params)
+  sResp.Result = Result
+  if err != nil {
+    sResp.ErrMsg = err.Error()
+  }
+
+  c.Data["json"] = sResp
+  c.ServeJSON()
+}
--- routers/router.go
+++ routers/router.go
@@ -7,4 +7,5 @@ import (
 
 func init() {
     beego.Router("/", &controllers.MainController{})
+    beego.Router("/api/service", &controllers.ServiceController{})
 }
